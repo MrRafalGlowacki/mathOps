88c6b588579244de0d2701b72cfe918d0a663696
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2023-01-22 15:44:51 +0100

    feature add: subtraction

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
 const firstNumber = document.querySelector(".firs-nmb");
 const secondNumber = document.querySelector(".second-nmb");
-const sign = document.querySelector(".type");
+const sign = document.querySelector(".sign");
 const form = document.querySelector(".form");
 const resultForm = document.querySelector('[name="result"]');
-// let generator = Math.round(Math.random() * 100);
+const changeBtn = document.querySelector(".change-btn");
+const type = document.querySelector(".type");
 let wynik = [];
 let goodResult;
-const reloadHandle = () => {
+
+const reloadHandlePlus = () => {
   firstNumber.textContent = Math.round(Math.random() * 100);
   secondNumber.textContent = Math.round(Math.random() * 100);
   const FN = Number(firstNumber.textContent);
   const SN = Number(secondNumber.textContent);
-
   goodResult = FN + SN;
   wynik.push(goodResult);
   console.log(wynik);
 };
-onload = () => {
-  reloadHandle();
+const reloadHandleMinus = () => {
+  firstNumber.textContent = Math.round(Math.random() * 100);
+  secondNumber.textContent = Math.round(Math.random() * 100);
+  const FN = Number(firstNumber.textContent);
+  const SN = Number(secondNumber.textContent);
+  goodResult = FN - SN;
+  wynik.push(goodResult);
+  console.log(wynik);
+};
+
+const typeHandler = () => {
+  if (changeBtn.textContent === "zmień na odejmowanie") {
+    changeBtn.textContent = "zmień na dodawanie";
+    type.textContent = "-";
+    reloadHandleMinus();
+    // console.log(wynik);
+  } else if (changeBtn.textContent === "zmień na dodawanie") {
+    changeBtn.textContent = "zmień na odejmowanie";
+    type.textContent = "+";
+    reloadHandlePlus();
+    // console.log(wynik);
+  } else return;
 };
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const result = Number(resultForm.value);
   if (result === goodResult) {
-    alert("good");
+    alert("Dobrze!");
+    if (type.textContent === "+") reloadHandlePlus();
+    else if (type.textContent === "-") reloadHandleMinus();
   } else {
-    alert("bad");
+    alert("Źle, spróbuj jeszcze raz");
+    form.reset();
+    return;
   }
-  console.log(result);
-  reloadHandle();
   form.reset();
 });
+
+changeBtn.addEventListener("click", typeHandler);
+onload = () => {
+  reloadHandlePlus();
+};
